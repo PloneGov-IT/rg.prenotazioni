dea6e2089e2a6aaf157efaea70a018321da8e9b4
Alessandro Pisa <alessandro.pisa@redturtle.it>
2013-07-10 18:28:57 +0200

    reallocate gate after moving

--- rg/prenotazioni/prenotazione_event.py
+++ rg/prenotazioni/prenotazione_event.py
@@ -5,9 +5,10 @@ from zope.interface import implements
 class IMovedPrenotazione(IObjectEvent):
     """Marker interface for prenotazione that is moved"""
 
+
 class MovedPrenotazione(ObjectEvent):
     """Event fired when a prenotazione that is moved"""
     implements(IMovedPrenotazione)
 
-    def __init__(self, object):
-        super(MovedPrenotazione, self).__init__(object)
+    def __init__(self, obj):
+        super(MovedPrenotazione, self).__init__(obj)
