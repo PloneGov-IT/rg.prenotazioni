ed6bc6178609d1c277d16666d8de34c899862c08
Alessandro Pisa <alessandro.pisa@redturtle.it>
2013-12-13 17:56:11 +0100

    Link to the calendar

--- rg/prenotazioni/browser/prenotazione.py
+++ rg/prenotazioni/browser/prenotazione.py
@@ -1,7 +1,33 @@
 # -*- coding: utf-8 -*-
 from Products.Five.browser import BrowserView
+from plone.memoize.view import memoize
+from urllib import urlencode
 
 
 class PrenotazioneView(BrowserView):
-
     """View for Prenotazione"""
+
+    @property
+    @memoize
+    def prenotazioni_folder(self):
+        ''' The parent prenotazioni folder
+        '''
+        return self.context.getPrenotazioniFolder()
+
+    @property
+    def booking_date(self):
+        ''' The parent prenotazioni folder
+        '''
+        return self.context.getData_prenotazione()
+
+    @property
+    @memoize
+    def back_url(self):
+        ''' Go back parent prenotazioni folder in the right day
+        '''
+        booking_date = self.booking_date
+        target = self.prenotazioni_folder.absolute_url()
+        if booking_date:
+            qs = {'data': booking_date.strftime('%d/%m/%Y')}
+            target = "%s?%s" % (target, urlencode(qs))
+        return target
