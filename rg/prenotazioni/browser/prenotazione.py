b01d2487884bc4ad49d87ee3c69375c46757898c
Alessandro Pisa <alessandro.pisa@redturtle.it>
2013-12-23 17:04:39 +0100

    View to fix bookings

--- rg/prenotazioni/browser/prenotazione.py
+++ rg/prenotazioni/browser/prenotazione.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from Products.Five.browser import BrowserView
 from plone.memoize.view import memoize
+from rg.prenotazioni.config import MIN_IN_DAY
 from urllib import urlencode
 
 
@@ -31,3 +32,21 @@ class PrenotazioneView(BrowserView):
             qs = {'data': booking_date.strftime('%d/%m/%Y')}
             target = "%s?%s" % (target, urlencode(qs))
         return target
+
+
+class ResetDuration(PrenotazioneView):
+    ''' Reset data scadenza prenotazione: sometime is needed :p
+    '''
+    def reset_duration(self):
+        ''' Reset the duration for this booking object
+        '''
+        tipology = self.context.getTipologia_prenotazione()
+        duration = self.prenotazioni.get_tipology_duration(tipology)
+        duration = (float(duration) / MIN_IN_DAY)
+        self.context.setData_scadenza(self.booking_date + duration)
+
+    def __call__(self):
+        ''' Reset the dates
+        '''
+        self.reset_duration()
+        return self.request.response.redirect(self.back_url)
