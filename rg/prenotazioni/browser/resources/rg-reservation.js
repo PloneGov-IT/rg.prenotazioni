375e0085794be1bd9c9ee22d7593c27d112dc8ef
Alessandro Pisa <alessandro.pisa@redturtle.it>
2014-01-24 15:46:46 +0100

    fromt end stuff

--- rg/prenotazioni/browser/resources/rg-reservation.js
+++ rg/prenotazioni/browser/resources/rg-reservation.js
@@ -1,1 +1,16 @@
-/*global jQuery, ajax_noresponse_message, close_box_message, window */
+/*globals jQuery, document*/
+/*jslint sloppy: true, vars: true, white: true, maxerr: 50, indent: 4 */
+/*
+ * This is the javascript that looks for overlays
+ */
+(function ($) {
+    /*
+    * Apply an overlay to all the links with class infoIco like
+    */
+    function prepOverlay() {
+        return $('a.prenotazioni-popup').prepOverlay({
+            subtype: 'ajax',
+        });
+    }
+    jq(document).ready(prepOverlay);
+}(jQuery));
