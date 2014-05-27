81307940f4727e9cc3a1a4c9d1905f0ec3741e52
Alessandro Pisa <alessandro.pisa@redturtle.it>
2014-05-27 14:18:59 +0200

    pep8

--- setup.py
+++ setup.py
@@ -9,48 +9,50 @@ version = '3.3.0.dev0'
 
 tests_require = ['zope.testing']
 
-setup(name='rg.prenotazioni',
-      version=version,
-      description="Booking product for Plone",
-      long_description=open("README.rst").read() + "\n" +
-      open(os.path.join("docs", "HISTORY.txt")).read(),
-      # Get more strings from
-      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
-      classifiers=[
-          'Development Status :: 3 - Alpha',
-          'Framework :: Plone',
-          'Framework :: Plone :: 4.0',
-          'Framework :: Plone :: 4.1',
-          'Framework :: Plone :: 4.2',
-          'Framework :: Plone :: 4.3',
-          'Programming Language :: Python',
-          'Programming Language :: Python :: 2.6',
-          'Programming Language :: Python :: 2.7',
-          'License :: OSI Approved :: GNU General Public License (GPL)',
-      ],
-      keywords='plone plonegov booking',
-      author='RedTurtle Technology',
-      author_email='sviluppoplone@redturtle.it',
-      url='http://plone.org/products/rg.prenotazioni',
-      license='GPL',
-      packages=find_packages(exclude=['ez_setup']),
-      namespace_packages=['rg', ],
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=['setuptools',
-                        'Products.DataGridField',
-                        'collective.contentrules.mailfromfield>0.2.0',
-                        'collective.fontawesome',
-                        'plone.api>=1.1.0',
-                        'pyinter',
-                        'quintagroup.formlib.captcha',
-                        ],
-      tests_require=tests_require,
-      extras_require=dict(tests=tests_require),
-      test_suite='rg.prenotazioni.tests.test_docs.test_suite',
-      entry_points="""
+setup(
+    name='rg.prenotazioni',
+    version=version,
+    description="Booking product for Plone",
+    long_description=open("README.rst").read() + "\n" +
+    open(os.path.join("docs", "HISTORY.txt")).read(),
+    # Get more strings from
+    # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Framework :: Plone',
+        'Framework :: Plone :: 4.0',
+        'Framework :: Plone :: 4.1',
+        'Framework :: Plone :: 4.2',
+        'Framework :: Plone :: 4.3',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+    ],
+    keywords='plone plonegov booking',
+    author='RedTurtle Technology',
+    author_email='sviluppoplone@redturtle.it',
+    url='http://plone.org/products/rg.prenotazioni',
+    license='GPL',
+    packages=find_packages(exclude=['ez_setup']),
+    namespace_packages=['rg', ],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+        'Products.DataGridField',
+        'collective.contentrules.mailfromfield>0.2.0',
+        'collective.fontawesome',
+        'plone.api>=1.1.0',
+        'pyinter',
+        'quintagroup.formlib.captcha',
+    ],
+    tests_require=tests_require,
+    extras_require=dict(tests=tests_require),
+    test_suite='rg.prenotazioni.tests.test_docs.test_suite',
+    entry_points="""
       # -*- entry_points -*-
       [z3c.autoinclude.plugin]
       target = plone
       """,
-      )
+)
